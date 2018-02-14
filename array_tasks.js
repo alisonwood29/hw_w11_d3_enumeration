ac513ebbb9063024e0007dbf91907093412d3f53
Alison Wood <alisonwood29@gmail.com>
2018-02-14 18:43:17 +0000

    test for concat function passing

---
+++ array_tasks.js
@@ -0,0 +1,48 @@
+const arrayTasks = {
+
+	concat: function (arr1, arr2) {
+    // const newArray = [];
+    // arr1.forEach(function (element) {
+    //   newArray.push(element);
+    // });
+    // arr2.forEach(function (element) {
+    //   newArray.push(element)
+    // });
+    // return newArray;
+    const newArray = arr1.concat(arr2);
+    return newArray;
+	},
+
+	// insertAt: function (arr, itemToAdd, index) {
+
+	// },
+
+	// square: function (arr) {
+
+	// },
+
+	// sum: function (arr) {
+
+	// },
+
+	// removeAndClone: function (arr, valueToRemove) {
+
+	// },
+
+	// findIndexesOf: function (arr, itemToFind) {
+
+	// },
+
+	// sumOfAllEvenNumbersSquared: function (arr) {
+
+	// }
+
+	// ----------- EXTENSION ------------
+
+	// findDuplicates: function (arr) {
+
+	// },
+
+}
+
+module.exports = arrayTasks
